ba8df5bc01f9d2bcfb1c62720bb2ad74b3019a46
Sanika500 <sanikasanthosh500@gmail.com>
2024-09-10 08:59:22 +0530

    done

--- mypro/myapp/models.py
+++ mypro/myapp/models.py
@@ -1,12 +1,12 @@
-# from django.db import models
-# class details (models.Model):
-#name=models.CharField(max_length=225)
+
 
 from django.db import models
 class  details(models.Model):
     name=models.CharField(max_length=50)
     email=models.CharField(max_length=50)
     password=models.CharField(max_length=50)
+
+
 
     
 
--- mypro/myapp/views.py
+++ mypro/myapp/views.py
@@ -1,25 +1,24 @@
-# from django.shortcuts import render,redirect
-# from .models import*
-# def index(request):
-#     if request.POST:
-#         title=request.POST.get('title')
-#         todoobj=todoitem(title=title)
+
+
+# from django.shortcuts import render
+# from.models import*
+# def index (request):
+#     if request.method=='POST':
 #         name=request.POST['name']
-#         obj=details(name=name)
+#         email=request.POST['email']
+#         password=request.POST['password']
+#         obj=details (name=name,email=email,password=password)
 #         obj.save()
-#         return redirect(index)
-# obj=details.objects.all()
+#     return render(request,'index.html')
 
-from django.shortcuts import render
-from.models import*
+
+from django.shortcuts import render,redirect
+from.models import *
 def index (request):
-    if request.method=='POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        password=request.POST['password']
-        obj=details (name=name,email=email,password=password)
-        obj.save()
-    return render(request,'index.html')
-
-
+    mylist=['apple','grape','orange','banana']
+    return render(request,'index.html',{'mylist':mylist})
+def about(request):
+    obj=todo.objects.all()
+    return render (request,'about.html',{'obj':obj})
+    
 
