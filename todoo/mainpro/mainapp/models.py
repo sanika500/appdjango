e8ada778e7634ed5b249d21fb567dadce9a717a1
Sanika500 <sanikasanthosh500@gmail.com>
2024-09-25 09:01:25 +0530

    done

--- todoo/mainpro/mainapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models her
-class details(models.Model):
-    todo=models.CharField(max_length=100)
--- todoo/mainpro/mainapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from.import views
-
-urlpatterns=[
-    path('',views.index,name="index"),
-    path('delete/<int:pk>/',views.delete_g,name='delete_g'),
-]
