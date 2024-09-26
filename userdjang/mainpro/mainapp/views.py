09b44147774de6a52df18940d4fe2ef9db215fcb
Sanika500 <sanikasanthosh500@gmail.com>
2024-09-26 06:13:08 +0530

    done

--- userdjang/mainpro/mainapp/views.py
+++ userdjang/mainpro/mainapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import user 
 
 # Create your views here.
+def index(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+    else:
+        return render(request,'index.html')
+def home(request):
+    return render(request,'home.html')
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+def register(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        email=request.POST['email']
+        user=user.objects.create_user(username=username,password=password,email=email)
+        user.save()
+        return redirect('index')
+    else:
+        return render(request,'register.html')
